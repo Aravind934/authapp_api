3c51749650ec835e1f2721f178d301a501a6da2f
Aravind roosevelt <55591267+Aravind934@users.noreply.github.com>
2022-09-29 10:39:40 -0700

    Update authenticate.js

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -3,11 +3,7 @@ const { verifyToken } = require("./verify");
 const authenticate = (req) => {
     try {
         let { token } = req.cookies;
-        console.log(">>>>>>>>>>>>>>>>>>>");
-        console.log(token)
         let result = verifyToken(token);
-        console.log(result)
-        console.log("<<<<<<<<<<<<<<<<<<<");
         if (!result) return false;
         return result.email;
     } catch (error) {
